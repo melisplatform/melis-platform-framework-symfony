97c4cf5b0ee87dd87ef7180a05ca1026a9284169
Cris <crismakabenta@gmail.com>
2019-10-07 19:55:59 +0800

    Check if framework already downloaded

--- install/scripts/dl-symfony-skeleton.php
+++ install/scripts/dl-symfony-skeleton.php
@@ -1,2 +1,14 @@
 <?php
-return MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('symfony');
+
+$isCliReqs = php_sapi_name() == 'cli' ? true : false;
+//third party Lumen
+$thirdPartyFolder = !$isCliReqs ? $_SERVER['DOCUMENT_ROOT'].DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'thirdparty/Symfony' : 'thirdparty/Symfony';
+
+if (!is_dir($thirdPartyFolder)) {
+    return MelisPlatformFrameworks\Support\MelisPlatformFrameworks::downloadFrameworkSkeleton('symfony');
+}else{
+    return [
+        'success' => true,
+        'message' => 'Symfony skeleton downloaded successfully'
+    ];
+}
